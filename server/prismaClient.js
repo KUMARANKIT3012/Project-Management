cb02b3708eb7528fd7b18e72311f49061f95d4e3
KUMARANKIT3012 <ankitkumar30122004@gmail.com>
2026-02-14 23:08:35 +0530

    update prisma client configuration

--- server/prismaClient.js
+++ server/prismaClient.js
@@ -1,23 +1,8 @@
-import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import { neonConfig } from '@neondatabase/serverless';
-import ws from 'ws';
-
-neonConfig.webSocketConstructor = ws;
 
 const globalForPrisma = globalThis;
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error('DATABASE_URL environment variable is not set');
-}
-
-const adapter = new PrismaNeon({ connectionString });
-
-const prisma = globalForPrisma.prisma ?? new PrismaClient({ 
-  adapter,
+const prisma = globalForPrisma.prisma ?? new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
 });
 
